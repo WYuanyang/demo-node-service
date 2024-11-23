04c8df25fd63a621322ae46bc7c34703ed18a33f
王远洋 <yuanyang613@163.com>
2024-11-24 01:33:43 +0800

    111

---
+++ server/config/wechat.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appId: 'xxxxxxxx',         // 测试号的 appID
+    appSecret: 'xxxxxxxxx', // 测试号的 appSecret
+    templateId: '0SHbzWAlHiuwo3Fw0ShFGFIKvocdgfWnujwvMGIoSPA',    // 验证码模版消息 ID
+    sendRedPacketTemplateId: '_nVo-iduu5oo4iRkGjYyxpuFR_B0uSARLKXbH_WkkSo',    // 红包模版消息 ID
+    testOpenId: 'xxxxxx'    // 测试用户的 OpenID
+}; 
